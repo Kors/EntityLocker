3f21a2d4d57dd2be217053a03472b7c91686e7fd
Kors <korsarsmail@mail.ru>
2018-02-03 21:06:42 +0300

    Add javadoc

--- src/main/java/locker/EntityLocker.java
+++ src/main/java/locker/EntityLocker.java
@@ -8,16 +8,33 @@ import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Supplier;
 
 /**
+ * Reusable utility class that provides synchronization mechanism similar to row-level DB locking.
+ *
+ * @param <T> type of id
  * @author Kors
  */
 public class EntityLocker<T> {
 
     private final ConcurrentMap<T, ReentrantLock> locks = new ConcurrentHashMap<>();
 
+    /**
+     * Method execute Supplier and return result, synchronized by id.
+     * Default time waiting for lock - 10 seconds.
+     *
+     * @see EntityLocker<>.modifyObject(T id, Supplier<R> action, long timeout)
+     */
     public <R> Optional<R> modifyObject(T id, Supplier<R> action) {
         return modifyObject(id, action, 10);
     }
 
+    /**
+     * Method execute Supplier and return result, synchronized by id.
+     * Allow the caller to specify timeout for locking an entity.
+     *
+     * @param id      id for synchronization
+     * @param action  operations that will be done synchronized
+     * @param timeout max time limit to wait lock
+     */
     public <R> Optional<R> modifyObject(T id, Supplier<R> action, long timeout) {
         ReentrantLock lock = getLock(id);
         try {
